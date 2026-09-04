dce461cd6dfe5a85863e43bfb418b0e2ee6f76b7
sahilshaikh00067 <sahilshaikh1582004@gmail.com>
2026-09-04 13:21:43 +0530

    Connect Django to PostgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,13 +134,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # DATABASE
 # =========================================================
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+import os
+import dj_database_url
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=False,
+    )
+}
 
 # =========================================================
 # PASSWORD VALIDATION
